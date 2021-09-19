10d19b29950b36d73f6422a01074c47b34f979c3
Rajesh Jha <jharajesh035@gmail.com>
2021-09-19 16:44:03 +0530

    firebase demo

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import Home from "./src/Container/Home"
+
+
+export default function App() {
+    return (
+      <View style={{flex:1}}>
+        <Home/>
+      </View>
+    );
+  }
+ 
+
